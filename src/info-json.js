670b252eed2f245a7c3adf5b3ef0c0619f994b17
Sushil Dhakal <sushildhakal1@gmail.com>
2021-09-13 20:17:55 +1000

    Add files via upload

---
+++ src/info-json.js
@@ -0,0 +1,70 @@
+
+
+const Information = [
+  { "label": 1,
+    "name":"Samule",
+    "age":21,
+    "country":"USA"
+  },
+  {"label": 2,
+    "name":"Sam",
+    "age":21,
+    "country":"USA"
+  },
+  {
+    "label": 3,
+    "name":"Mark",
+    "age":21,
+    "country":"Africa"
+  },
+  {
+    "label": 4,
+    "name":"Markus",
+    "age":21,
+    "country":"Africa"
+  },
+  {
+    "label": 5,
+    "name":"Aayush",
+    "age":21,
+    "country":"India"
+  },
+  {
+    "label": 6,
+    "name":"Sean",
+    "age":21,
+    "country":"Ireland"
+  },
+  {
+    "label": 7,
+    "name":"Eduardo",
+    "age":21,
+    "country":"France"
+  },
+  {
+    "label": 8,
+    "name":"Dustin",
+    "age":21,
+    "country":"Spain"
+  },
+  {
+    "label": 9,
+    "name":"Alexendra",
+    "age":21,
+    "country":"USA"
+  },
+  {
+    "label": 10,
+    "name":"Lee",
+    "age":21,
+    "country":"China"
+  },
+  {
+    "label": 11,
+    "name":"Jim",
+    "age":21,
+    "country":"Korea"
+  }
+];
+
+export default Information;
